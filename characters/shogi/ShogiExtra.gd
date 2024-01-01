e59f6926f2b52e23267bb55e11ab3a4c2c331123
hannuwu <brooje.lasher@gmail.com>
2024-01-01 05:35:32 -0500

    conquer mechanic

--- characters/shogi/ShogiExtra.gd
+++ characters/shogi/ShogiExtra.gd
@@ -1,12 +1,14 @@
 extends PlayerExtra
 
 func _ready():
-	$"%DropButton".connect("pressed", self, "emit_signal", ["data_changed"])
+	$"%DropButton".connect("item_selected", self, "emit_signal", ["data_changed"])
+	$"%ConquerSelect".connect("data_changed", self, "emit_signal", ["data_changed"])
 	
 
 func get_extra():
 	return {
 		"drop_enabled":$"%DropButton".pressed,
+		"conquer_tier":$"%ConquerSelect".selected
 	}
 
 func update_selected_move(move_state):
@@ -18,5 +20,17 @@ func show_options():
 	else:
 		$"%DropButton".show()
 
+func _process(delta):
+	if not (selected_move is preload("res://_Shogi/characters/Shogi/states/ShogiState.gd")):
+		$"%ConquerSelect".hide()
+	else:
+		print(selected_move.can_conquer)
+		if not selected_move.can_conquer:
+			$"%ConquerSelect".hide()
+			$"%ConquerSelect".selected = 0
+		else:
+			$"%ConquerSelect".show()
+
 func reset():
 	$"%DropButton".set_pressed_no_signal(false)
+	$"%ConquerSelect".selected = 0
