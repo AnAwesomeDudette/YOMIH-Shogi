6d4f78cc39068b1e1689e530803fddbd26e8a92e
AnAwesomeDudette <akajiakaji@gmail.com>
2023-12-31 23:00:46 -0600

    This time for sure
    Fixed resource pathways, so that the files are actually usable.

--- _Shogi/characters/Shogi/states/Split.gd
+++ _Shogi/characters/Shogi/states/Split.gd
@@ -1,7 +1,15 @@
 extends "res://_Shogi/characters/Shogi/states/ShogiState.gd"
 
-onready var flurry1 = $Flurry1
-onready var flurry2 = $Flurry2
-onready var ender = $Ender
+onready var a1 = $A1
+onready var a2 = $A2
+onready var a3 = $A3
+onready var b1 = $B1
+onready var b2 = $B2
 
-
+func _enter():
+	._enter()
+	_add(a1, 2, 2)
+	_add(a2, 2, 2)
+	_add(a3, 2, 2)
+	_add(b1, 2, 2)
+	_add(b2, 2, 2)
