4fca3ea09fdec17295768e552a73593eb5d7f44a
Trini-Tea <xanosdagamer@gmail.com>
2024-01-08 17:27:11 -0500

    Revamped Hustle

---
+++ characters/shogi/ReminisceTaunt.gd
@@ -0,0 +1,25 @@
+extends CharacterState
+
+var super_level = 4
+onready var TauntSong = $"%ShogiTaunt"
+
+
+func _ready():
+	pass
+
+func _enter():
+	host.play_sound("ShogiTaunt")
+
+func _tick():
+	host.gain_super_meter(6)
+	#print(TauntSong.playing)
+
+func _frame_44():
+	host.unlock_achievement("ACH_HUSTLE", true)
+
+func is_usable():
+	var new_usable = host.supers_available >= super_level and host.Fear >= host.Fear_AMOUNT
+	return .is_usable() and (not new_usable or host.install_active)
+
+func _exit():
+	host.stop_sound("ShogiTaunt")
---
+++ characters/shogi/TauntShogi.gd
@@ -0,0 +1,10 @@
+extends CharacterState
+
+var super_level = 4
+
+func _ready():
+	pass
+
+func _frame_44():
+	host.gain_super_meter(host.MAX_SUPER_METER)
+	host.unlock_achievement("ACH_HUSTLE", true)
