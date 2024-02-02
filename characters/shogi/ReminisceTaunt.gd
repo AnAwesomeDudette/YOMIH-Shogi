6bea809fdcd074ab2477a8bcf882958a3141777c
Trini-Tea <xanosdagamer@gmail.com>
2024-02-02 15:11:56 -0500

    Even more fixes + Sadness oversight removed on taunt

--- characters/shogi/ReminisceTaunt.gd
+++ characters/shogi/ReminisceTaunt.gd
@@ -10,13 +10,15 @@ func _ready():
 func _enter():
 	if not $"%ShogiTaunt".playing:
 		host.play_sound("ShogiTaunt")
+	host.queue_damage_multiplier = 0.7
 
 
 
 func _tick():
-	host.gain_super_meter(6)
+	host.gain_super_meter(10)
 	#print(TauntSong.playing)
 	host.start_projectile_invulnerability()
+	host.penalty = 25
 
 func _frame_44():
 	host.unlock_achievement("ACH_HUSTLE", true)
@@ -24,10 +26,6 @@ func _frame_44():
 func _frame_70():
 	if host.opponent.current_state().endless == true:
 		host.opponent.current_state().enable_interrupt()
-
-func is_usable():
-	var new_usable = host.supers_available >= super_level and host.Fear >= host.Fear_AMOUNT
-	return .is_usable() and (not new_usable or host.install_active)
 
 func _exit():
 	host.stop_sound("ShogiTaunt")
