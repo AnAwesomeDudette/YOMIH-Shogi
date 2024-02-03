ab05691ebb79334dfd0f95ae09a45dcbb772274b
Trini-Tea <xanosdagamer@gmail.com>
2024-02-02 20:26:15 -0500

    some optimizations

--- characters/shogi/Win.gd
+++ characters/shogi/Win.gd
@@ -5,3 +5,4 @@ extends CharacterState
 func _frame_0():
 	$"./Dialogue".show()
 	$"./Dialogue".text = host.Dialogue.GetLine(host.getOpponentName(), "Win", Network.pid_to_username(host.opponent.id))
+
